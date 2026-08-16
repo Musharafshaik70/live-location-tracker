201ed2b9d1d6fbca6c5adad9b0236f900467cc79
Musharaf Shaik <musharafshaik630@gmail.com>
2026-08-17 01:58:56 +0530

    chore:initialize live location tracker

---
+++ index.js
@@ -0,0 +1,31 @@
+import http from "node:http";
+import path from "node:path";
+
+import express from "express";
+import { Server } from "socket.io";
+
+async function main() {
+    const PORT = process.env.PORT ?? 9000;
+
+    const app = express();
+    const httpServer = http.createServer(app);
+    const io = new Server();
+
+    io.attach(httpServer);
+    //socket.io handlers
+    io.on("connection", (socket) => {
+        console.log(`Socket - ${socket.id} connected`);
+    });
+
+    //express handlers
+    app.use(express.static(path.resolve("./public")));
+    app.get("/health", (req, res) => {
+        return res.status(200).json({ healthy: true });
+    });
+
+    httpServer.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+main();
